896402b502740c8e2fb39dba91f50e46611bef19
donnate <cdonnat@uchicago.edu>
2025-03-03 15:17:49 -0400

    change path

--- src/final_simulation.py
+++ src/final_simulation.py
@@ -289,8 +289,8 @@ for it_exp, exp in enumerate([args.seed]):
                                                         "smoothness_max", "smoothness_cor",
                                                         "Method", "L","fold", "Error", "Prediction",
                                                         "Bias", "Variance"])
-                    df_train.to_csv("results/new_results2_non_linearity_train_simu_latent_topo_NL" + str(args.non_linear) + "_seed" +  str(args.seed)+  "_add_nodes"+str(args.add_node)  + graph_type  + "_prediction.csv")
-                    df_test.to_csv("results/new_results2_non_linearity_test_simu_latent_topo_NL" + str(args.non_linear) + "_seed" +   str(args.seed)+  "_add_nodes"+str(args.add_node)  + graph_type  + "_prediction.csv")
+                    df_train.to_csv("results/final_results/simu_NL" + str(args.non_linear) + "_seed" +  str(args.seed)+  "_add_nodes"+str(args.add_node) + "_"  + graph_type  + "_prediction.csv")
+                    df_test.to_csv("results/final_results/simu_NL" + str(args.non_linear) + "_seed" +   str(args.seed)+  "_add_nodes"+str(args.add_node) + "_"  + graph_type  + "_prediction.csv")
 
 
                 
